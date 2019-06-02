9729fd3a7c0699e535093c348f4402aaa5dd3aa3
Megan Wu <meganwu1994@gmail.com>
2019-06-01 20:04:45 -0500

    just started js

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+
+    // randomly generate a number, put it into #random-box
+    var randomNumber = Math.floor(Math.random() * 100) + 1;
+    $("#random").text(randomNumber);
+
+
+
+    // win = 0, lose = 0
+
+
+
+    // make each crystal represent a number
+
+
+
+    // when clicked, add up the numbers and show it in #score-box
+
+
+
+    // if the number added up === the random number, win!
+
+
+
+    // if the number added up > the random number, lose!
+
+
+}
