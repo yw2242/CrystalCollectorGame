b790070f679aa720dd2a80cdf248ba7bd5e33667
Megan Wu <meganwu1994@gmail.com>
2019-06-03 22:41:38 -0500

    score problem fixed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,177 @@
 $(document).ready(function() {
 
 
-    // randomly generate a number, put it into #random-box
-    var randomNumber = Math.floor(Math.random() * 100) + 1;
-    $("#random").text(randomNumber);
 
 
 
-    // win = 0, lose = 0
-    var wins = 0;
-    var losses = 0;
-    var score = 0;
+  // randomly generate a number, put it into #random-box
 
-    var winsText = document.getElementById("win");
-    var lossesText = document.getElementById("lose");
-    var totalScore = document.getElementById("score");
+  var random = Math.floor(Math.random() * 100) + 1;
 
-
-    // make each crystal represent a number
-    var valueRed = 5;
-    var valueYellow = 1;
-    var valueGreen = 10;
-    var valueBlue = 2;
+  $("#random").text(random);
 
 
 
-    // when clicked, add up the numbers and show it in #score-box
-    $("#red").on("click", function() {
-        score = score + valueRed;
-        console.log(score);
-         // return score;
-       $("#score").text(score);
-       [wins, losses, score] = checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score);
+  // win = 0, lose = 0
 
-      });
+  var wins = 0;
 
-      $("#yellow").on("click", function() {
-        score = score + valueYellow;
-        console.log(score);
-        // return score;
-        $("#score").text(score);
-        [wins, losses, score] = checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score);
+  var losses = 0;
 
-      });
-
-      $("#green").on("click", function() {
-        score = score + valueGreen;
-        console.log(score);
-        // return score;
-        $("#score").text(score);
-        [wins, losses, score] = checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score);
-
-      });
-
-      $("#blue").on("click", function() {
-        score = score + valueBlue;
-        console.log(score);
-        // return score;
-        $("#score").text(score);
-        [wins, losses, score] = checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score);
+  var score = 0;
 
 
-      });
+
+  var winsText = document.getElementById("win");
+
+  var lossesText = document.getElementById("lose");
+
+  var totalScore = document.getElementById("score");
 
 
-      function checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score) {
+
+  // make each crystal represent a number
+
+  var valueRed = 5;
+
+  var valueYellow = 1;
+
+  var valueGreen = 10;
+
+  var valueBlue = 2;
+
+
+
+  // when clicked, add up the numbers and show it in #score-box
+
+  $("#red").on("click", function() {
+
+      score = score + valueRed;
+
+      console.log(score);
+
+      // return score;
+
+      $("#score").text(score);
+
+      [wins, losses, score, random] = checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score);
+
+
+
+  });
+
+
+
+  $("#yellow").on("click", function() {
+
+      score = score + valueYellow;
+
+      console.log(score);
+
+      // return score;
+
+      $("#score").text(score);
+
+      [wins, losses, score, random] = checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score);
+
+
+
+  });
+
+
+
+  $("#green").on("click", function() {
+
+      score = score + valueGreen;
+
+      console.log(score);
+
+      // return score;
+
+      $("#score").text(score);
+
+      [wins, losses, score, random] = checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score);
+
+
+
+  });
+
+
+
+  $("#blue").on("click", function() {
+
+      score = score + valueBlue;
+
+      console.log(score);
+
+      // return score;
+
+      $("#score").text(score);
+
+      [wins, losses, score, random] = checkWin(score, random, winsText, wins, lossesText, losses, totalScore, score);
+
+
+
+
+
+  });
+
+
+
+  function checkWin(score, random, winsText, wins, lossesText, losses, totalScore) {
+
+
+
+      // if the number added up === the random number, win!
+
+      console.log("Hi");
+
+      console.log("score = " + score);
+
+      console.log("random = " + random);
       
-        // if the number added up === the random number, win!
-      
-          if (score === random) {
-              wins++;
-      
-      
-          // if the number added up > the random number, lose!
-      
-          } else if (score > random) {
-              loses++;
-          }
-      
-          winsText.textContent = "wins: " + wins;
-          lossesText.textContent = "losses: " + losses;
-          totalScore.textContent = score;
+      if (score >= random) {
 
-          return [wins, losses, score];
-      
-      }
+        if (score == random) {
+
+            wins++;
+
+            console.log("Hi score == random");
+
+            // if the number added up > the random number, lose!
+
+        } else if (score > random) {
+
+            losses++;
+
+            console.log("Hi score > random");
+
+        }
+
+        random = Math.floor(Math.random() * 100) + 1;
+
+        $("#random").text(random);
+
+        score = 0;
+
+     }
+
+
+
+      winsText.textContent = "wins: " + wins;
+
+      lossesText.textContent = "losses: " + losses;
+
+      totalScore.textContent = score;
+
+
+
+      return [wins, losses, score, random];
+
+
+
+  }
+
 
 
 });
